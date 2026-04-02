dec9298c9f2e23c0cff41031cfb60d2d9af16cb5
Shona S1 <mark111allen@gmail.com>
2026-04-02 17:46:44 -0600

    Testing github nothing crazy

--- testing.py
+++ testing.py
@@ -3,6 +3,7 @@ import pyqtgraph as pg
 from PyQt6 import QtWidgets
 import numpy as np
 import sys
+from sympy.parsing.latex import parse_latex
 app=QtWidgets.QApplication(sys.argv)
 window=QtWidgets.QMainWindow()
 window.resize(800,600)
@@ -11,10 +12,15 @@ plot_widget=pg.PlotWidget()
 window.setCentralWidget(plot_widget)
 
 x=np.linspace(-10,10,100)
-y=x**2
 
+expression=parse_latex("x^2")
+
+functions=[expression]
+
+for item in functions:
+    plot_widget.plot(x,item)
+plot_widget.setXRange(-10,10)
+plot_widget.setYRange(-1,10)
 plot_widget.showGrid(x=True,y=True)
-
-plot_widget.plot(x,y)
 window.show()
 sys.exit(app.exec())
