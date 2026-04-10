e71a057cd79a2c2a50dc2fb7cf1cd29ea6ff72d2
Shona S1 <mark111allen@gmail.com>
2026-04-09 17:00:55 -0700

    I dont even know

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pyqtgraph as pg
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets,QtCore,QtGui, uic
+import pyqtgraph.opengl as gl
 import numpy as np
 import sys
 from sympy.parsing.latex import parse_latex
@@ -9,7 +10,10 @@ app=QtWidgets.QApplication(sys.argv)
 window=QtWidgets.QMainWindow()
 window.resize(800,600)
 
+sidebar=uic.loadUi(r"C:\Users\Mark\Documents\The Project\test.ui",window)
+
 plot_widget=pg.PlotWidget()
+sidebar= QtWidgets.QWidget()
 window.setCentralWidget(plot_widget)
 array=np.linspace(-10,10,100)
 #Array that acts as the x values for the plot
@@ -25,7 +29,8 @@ for item in functions:
     plot_widget.plot(array,f(array))
     #Plot the array as x, and evaluate the function for each value in the array
 plot_widget.setXRange(-10,10)
-plot_widget.setYRange(-1,10)
+plot_widget.setYRange(-10,10)
 plot_widget.showGrid(x=True,y=True)
+
 window.show()
 sys.exit(app.exec())
